28d3ed041d4055ee3f663fb95b07c32a3c7efa6c
kieranwatt <kieranmwatt@gmail.com>
2024-01-16 09:26:16 -0500

    yoooo

---
+++ array.js
@@ -0,0 +1,37 @@
+const mewmax = [
+    {
+        firstname: "Corange",
+        lastname: "Porange",
+        age: 12,
+        city: "Cleveland",
+        hometown: "Bed-Stuy, NY",
+        cool: false,
+        favoritenumbers: ["1","3"],
+    },
+
+
+
+    // 1. True or false: Mewing improves facial health.
+    
+    // 2. True or false: A lack of mewing causes facial deformities.
+    
+    // 3. True or false: Mewing can change one's facial structure.
+    
+    // 4. True or false: A lack of mewing causes respiratory issues.
+    
+    // 5. True or false: Mewing helps with swallowing more food.
+    
+    // 6. True or false: It's important to mew while eating.
+
+    
+// True
+
+// False
+
+// True
+
+// False
+
+// False
+
+// True
